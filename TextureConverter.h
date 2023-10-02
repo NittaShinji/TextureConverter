3444abf63be102deae1111f336e11ff011139b09
Nittshinji <94505316+NittaShinji@users.noreply.github.com>
2023-10-02 15:23:42 +0900

    02_04　テクスチャファイル読み書き1完了

---
+++ TextureConverter.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <string>
+
+//テクスチャコンバーター
+class TextureConverter
+{
+
+public:
+
+	/// <summary>
+	/// テクスチャをWICからDDSに変換する
+	/// </summary>
+	/// <param name="filepath">ファイルパス</param>
+	void ConvertTextureWICToDDS(const std::string& filepath);
+
+private:
+
+	/// <summary>
+	/// テクスチャファイル読み込み
+	/// </summary>
+	/// <param name="filePath">ファイルパス</param>
+	void LoadWICTextureFromFile(const std::string& filePath);
+
+	/// <summary>
+	/// マルチバイト文字列をワイド文字列に変換
+	/// </summary>
+	/// <param name="mString">マルチバイト文字列</param>
+	/// <returns>ワイド文字列</returns>
+	static std::wstring ConvertMultiByteStringToWideString(const std::string& mString);
+
+};
+
--- main.cpp
+++ main.cpp
@@ -1,17 +1,42 @@
 #include <cstdio>
 #include <cstdlib>
+//#include <cstdint>
 #include <DirectXTex.h>
+#include "TextureConverter.h"
+
+//コマンドライン引数
+enum Argument
+{
+	kApplicationPath,	//アプリケーションのパス
+	kFilePath,			//渡されたファイルのパス
+
+	NumArgument
+};
 
 int main(int argc, char* argv[])
 {
-	//argcの数だけ繰り返す
-	for(int i = 0; i < argc; i++)
-	{
-		//文字列argvのi版を表示
-		printf(argv[i]);
-		//改行
-		printf("\n");
-	}
+	assert(argc >= NumArgument);
+
+	//COMライブラリの初期化
+	HRESULT hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
+	assert(SUCCEEDED(hr));
+
+	//テクスチャコンバータ
+	TextureConverter converter;
+
+	//テクスチャ変換
+	converter.ConvertTextureWICToDDS(argv[kFilePath]);
+
+	//COMライブラリの終了
+	CoUninitialize();
+
+	//for(int i = 0; i < argc; i++)
+	//{
+	//	//文字列argvのi版を表示
+	//	printf(argv[i]);
+	//	//改行
+	//	printf("\n");
+	//}
 
 	system("pause");
 	return 0;
